ed202c2b939d161095ccc09c30eecb0de23f7e15
chungyin0830 <cles0890545@gmail.com>
2025-04-23 11:58:21 +0800

    Lab1

---
+++ Lab1.cs
@@ -0,0 +1,47 @@
+﻿namespace Lab1
+{
+    internal class Lab1
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int x = 0;int y = 99;
+            int Ans = random.Next(x, y);
+            Console.WriteLine("({0},{1})", x, y);
+            int Num = int.Parse(Console.ReadLine());
+
+            while (Ans != Num)
+            {
+                if (Num >= x && Num <= y)
+                {
+                    if (Num < Ans)
+                    {
+                        x = Num+1;
+                    }
+                    else
+                    {
+                        y = Num-1;
+                    }
+                    if (y-x < 1)
+                    {
+                        break;
+                    }  
+                }
+                else 
+                {
+                    Console.WriteLine("Out of Range, Try Again?");
+                }
+                Console.WriteLine("({0},{1})", x, y);
+                Num = int.Parse(Console.ReadLine());
+            }
+            if (Ans == Num)
+            {
+                Console.WriteLine("Bingo!");
+            }
+            else 
+            { 
+                Console.WriteLine("GG"); 
+            } 
+        }
+    }
+}
