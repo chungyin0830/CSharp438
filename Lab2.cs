a0838799a9864fa974f7f40307d24dc81b3319ce
chungyin0830 <cles0890545@gmail.com>
2025-05-11 20:55:01 +0800

    Add files via upload

---
+++ Lab2.cs
@@ -0,0 +1,54 @@
+﻿namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number of citizens:");
+            int N = int.Parse(Console.ReadLine());
+            int[] contacts = new int[N];
+            Console.Write("Id :         ");
+            for (int i = 0; i < N; i++)
+            {
+                contacts[i] = i;
+                Console.Write("{0,  3}", contacts[i]);
+            }
+
+            Console.WriteLine();
+
+            Random rng = new Random();
+            Console.Write("Contactee :  ");
+            for (int i = N - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                int temp = contacts[i];
+                contacts[i] = contacts[j];
+                contacts[j] = temp;
+            }
+ 
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("{0,  3}", contacts[i]);
+            }
+            Console.WriteLine("\n------------------------");
+
+            List<int> chain = new List<int>();
+            HashSet<int> visited = new HashSet<int>();
+            Console.WriteLine("\nEnter id of infected citizen:");
+            int current = int.Parse(Console.ReadLine());
+            while (!visited.Contains(current))
+            {
+                chain.Add(current);
+                visited.Add(current);
+                current = contacts[current];
+            }
+            Console.WriteLine("These citizens are to be self-isolated in the following 14 days:");
+            foreach (int id in chain)
+            {
+                Console.Write(id + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab3.cs
@@ -0,0 +1,68 @@
+﻿namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number of columns: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nMatrix after filling with random values: ");
+            int total = rows * cols;
+            int[] numbers = new int[total];
+            for (int i = 0; i < total; i++)
+            {
+                numbers[i] = i;
+            }
+
+            Random rand = new Random();
+            int[] array = numbers;
+            for (int ii = array.Length - 1; ii > 0; ii--)
+            {
+                int jj = rand.Next(ii + 1);
+                (array[ii], array[jj]) = (array[jj], array[ii]);
+            }
+
+            int[,] matrix = new int[rows, cols];
+            int index = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = numbers[index++];
+                    Console.Write("{0, 4}",matrix[i, j].ToString());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nMatrix after performing row exchanges based on the first column: ");
+
+            int matrix_rows = matrix.GetLength(0);
+            int matrix_cols = matrix.GetLength(1);
+
+            for (int i = 0; i < matrix_rows - 1; i++)
+            {
+                for (int j = i + 1; j < matrix_rows; j++)
+                {
+                    if (matrix[i, 0] > matrix[j, 0])
+                    {
+                        for (int k = 0; k < matrix_cols; k++)
+                        {
+                            (matrix[i, k], matrix[j, k]) = (matrix[j, k], matrix[i, k]);
+                        }
+                    }
+                }
+            }
+            int rows2 = matrix.GetLength(0);
+            int cols2 = matrix.GetLength(1);
+            for (int i = 0; i < rows2; i++)
+            {
+                for (int j = 0; j < cols2; j++)
+                    Console.Write("{0, 4}",matrix[i, j].ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
